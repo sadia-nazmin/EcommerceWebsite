6a93b7285a521f8b7a74d135610aac11f5bc53b7
Sadia Nazmin <sadianazmin228@gmail.com>
2023-12-20 21:41:02 +0600

    creating nav-bar component  & product interface

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Product } from './models/product';
+import { Pagination } from './models/pagination';
 
 @Component({
   selector: 'app-root',
@@ -8,15 +10,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   title = 'Skinet';
+  products: Product[] = [];
   constructor(private http: HttpClient) {}
   ngOnInit(): void {
-    this.http.get('https://localhost:5001/api/products').subscribe({
-      next: (response) => console.log(response), //what to do next
-      error: (error) => console.log(error), //what to do if there is an error
-      complete: () => {
-        console.log('request completed');
-        console.log('extra statement');
-      },
-    });
+    this.http
+      .get<Pagination<Product[]>>(
+        'https://localhost:5001/api/products?pageSize=50'
+      )
+      .subscribe({
+        next: (response) => (this.products = response.data), //what to do next
+        error: (error) => console.log(error), //what to do if there is an error
+        complete: () => {
+          console.log('request completed');
+          console.log('extra statement');
+        },
+      });
   }
 }
