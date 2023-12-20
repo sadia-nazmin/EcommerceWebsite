7eb630e332da88842b6c851d23a5c7490967959d
Sadia Nazmin <sadianazmin228@gmail.com>
2023-12-20 19:27:30 +0600

    certificate error persist

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Skinet';
+  constructor(private http: HttpClient) {}
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/products').subscribe({
+      next: (response) => console.log(response), //what to do next
+      error: (error) => console.log(error), //what to do if there is an error
+      complete: () => {
+        console.log('request completed');
+        console.log('extra statement');
+      },
+    });
+  }
 }
