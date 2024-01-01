aa946ed81f83308abd060572df4960380958f0a8
Sadia Nazmin <sadianazmin228@gmail.com>
2024-01-01 18:50:07 +0600

    building UI & routing & lazy  loading

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,16 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HttpClientModule } from '@angular/common/http';
+import { CoreModule } from './core/core.module';
+import { HomeModule } from './home/home.module';
 
 @NgModule({
-  declarations: [AppComponent, NavBarComponent],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
+    CoreModule,
+    HomeModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
